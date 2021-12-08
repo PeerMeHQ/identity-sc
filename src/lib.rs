05b0898719750efded602229a652bf8a6b18b419
micha vie <micha@freeiam.com>
2021-12-08 21:27:30 +0100

    update set image

--- src/lib.rs
+++ src/lib.rs
@@ -3,44 +3,49 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
-#[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, TypeAbi, Clone)]
+#[derive(TopEncode, TopDecode, TypeAbi, Clone)]
 pub struct ImageNft<M: ManagedTypeApi> {
-    pub token_type: TokenIdentifier<M>,
+    pub token_id: TokenIdentifier<M>,
     pub nonce: u64,
 }
 
 #[elrond_wasm::contract]
 pub trait Identity {
     #[init]
-    fn init(&self, cost_token: TokenIdentifier) {
+    fn init(&self, cost_token: TokenIdentifier, image_update_cost: BigUint) {
         self.cost_token_id().set(&cost_token);
+        self.image_update_cost_amount().set(&image_update_cost);
     }
 
     #[payable("*")]
     #[endpoint(setImage)]
     fn set_image(
         &self,
-        #[payment_token] cost_token: TokenIdentifier,
+        #[payment_token] cost_token_id: TokenIdentifier,
         #[payment_amount] cost_amount: BigUint,
         nft_id: TokenIdentifier,
         nft_nonce: u64,
     ) -> SCResult<()> {
         require!(nft_id.is_valid_esdt_identifier(), "not an nft");
+        require!(cost_token_id == self.cost_token_id().get(), "invalid token");
+        require!(cost_amount >= self.image_update_cost_amount().get(), "invalid amount");
 
         let caller = self.blockchain().get_caller();
         let image_nft = ImageNft {
-            token_type: nft_id,
+            token_id: nft_id,
             nonce: nft_nonce,
         };
 
         self.image_nfts_by_address(&caller).set(&image_nft);
-        self.send().esdt_local_burn(&cost_token, 0, &cost_amount);
 
         Ok(())
     }
 
     #[storage_mapper("costTokenId")]
     fn cost_token_id(&self) -> SingleValueMapper<TokenIdentifier>;
+
+    #[storage_mapper("costImageUpdateAmount")]
+    fn image_update_cost_amount(&self) -> SingleValueMapper<BigUint>;
 
     #[storage_mapper("imagesNfts")]
     fn image_nfts_by_address(&self, address: &ManagedAddress) -> SingleValueMapper<ImageNft<Self::Api>>;
---
+++ tests/set_image_tests.rs
@@ -0,0 +1,82 @@
+elrond_wasm::imports!();
+
+use elrond_wasm_debug::{assert_sc_error, managed_address, managed_biguint, managed_token_id, rust_biguint, testing_framework::*};
+use identity::{self, Identity};
+
+const SC_WASM_PATH: &'static str = "../output/adder.wasm";
+const COST_TOKEN_ID: &[u8] = b"COST-abcdef";
+
+#[test]
+fn it_stores_nft_as_image() {
+    let mut blockchain = BlockchainStateWrapper::new();
+    let caller = blockchain.create_user_account(&rust_biguint!(1_000));
+    let contract = blockchain.create_sc_account(&rust_biguint!(0), None, identity::contract_obj, SC_WASM_PATH);
+
+    blockchain.set_esdt_balance(&caller, COST_TOKEN_ID, &rust_biguint!(1_000));
+
+    blockchain.execute_esdt_transfer(&caller, &contract, COST_TOKEN_ID, 0, &rust_biguint!(100), |sc| {
+        sc.init(managed_token_id!(COST_TOKEN_ID), managed_biguint!(100));
+
+        let result = sc.set_image(
+            managed_token_id!(COST_TOKEN_ID),
+            managed_biguint!(100),
+            managed_token_id!(b"COL-123456"),
+            2,
+        );
+
+        assert_eq!(result, SCResult::Ok(()));
+
+        StateChange::Commit
+    });
+
+    blockchain.execute_query(&contract, |sc| {
+        let actual = sc.image_nfts_by_address(&managed_address!(&caller)).get();
+        assert_eq!(managed_token_id!(b"COL-123456"), actual.token_id);
+        assert_eq!(2, actual.nonce);
+    });
+}
+
+#[test]
+fn it_fails_when_paid_with_wrong_cost_token() {
+    let mut blockchain = BlockchainStateWrapper::new();
+    let caller = blockchain.create_user_account(&rust_biguint!(1_000));
+    let contract = blockchain.create_sc_account(&rust_biguint!(0), None, identity::contract_obj, SC_WASM_PATH);
+
+    blockchain.set_esdt_balance(&caller, COST_TOKEN_ID, &rust_biguint!(1_000));
+
+    blockchain.execute_esdt_transfer(&caller, &contract, COST_TOKEN_ID, 0, &rust_biguint!(100), |sc| {
+        sc.init(managed_token_id!(COST_TOKEN_ID), managed_biguint!(100));
+
+        let result = sc.set_image(
+            managed_token_id!(b"INVALID-abcdef"),
+            managed_biguint!(100),
+            managed_token_id!(b"TEST-123456"),
+            0,
+        );
+
+        assert_sc_error!(result, b"invalid token");
+
+        StateChange::Commit
+    });
+}
+
+#[test]
+fn it_fails_when_paid_amount_is_less_than_cost_amount() {
+    let mut blockchain = BlockchainStateWrapper::new();
+    let caller = blockchain.create_user_account(&rust_biguint!(1_000));
+    let contract = blockchain.create_sc_account(&rust_biguint!(0), None, identity::contract_obj, SC_WASM_PATH);
+
+    blockchain.set_esdt_balance(&caller, COST_TOKEN_ID, &rust_biguint!(1_000));
+
+    blockchain.execute_esdt_transfer(&caller, &contract, COST_TOKEN_ID, 0, &rust_biguint!(100), |sc| {
+        let cost_token = managed_token_id!(COST_TOKEN_ID);
+
+        sc.init(cost_token.clone(), managed_biguint!(100));
+
+        let result = sc.set_image(cost_token, managed_biguint!(90), managed_token_id!(b"TEST-123456"), 0);
+
+        assert_sc_error!(result, b"invalid amount");
+
+        StateChange::Commit
+    });
+}
